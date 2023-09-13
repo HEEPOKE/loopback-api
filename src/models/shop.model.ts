ee2fa2a4768e3ee774c27697449fdee1eee89e3c
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-09-13 15:31:20 +0700

    :money_with_wings: update shop model

--- src/models/shop.model.ts
+++ src/models/shop.model.ts
@@ -1,30 +1,58 @@
 import {Entity, model, property} from '@loopback/repository';
-import { StatusEnum } from '../enum/status.enum';
+import {StatusEnum} from '../enum/status.enum';
 
-@model()
+@model({
+  settings: {
+    postgresql: {schema: 'public', table: 'shop'},
+  },
+})
 export class Shop extends Entity {
   @property({
     type: 'number',
     id: true,
     generated: true,
+    description: 'The unique identifier for the shop.',
+    postgresql: {
+      columnName: 'id',
+      dataType: 'serial',
+      nullable: 'NO',
+    },
   })
-  id?: number;
+  id: number;
 
   @property({
     type: 'string',
     required: true,
+    description: 'The name of the shop.',
+    postgresql: {
+      columnName: 'name',
+      dataType: 'text',
+      nullable: 'NO',
+    },
   })
   name: string;
 
   @property({
     type: 'string',
     required: true,
+    description: 'A description of the shop.',
+    postgresql: {
+      columnName: 'description',
+      dataType: 'text',
+      nullable: 'NO',
+    },
   })
   description: string;
 
   @property({
     type: 'string',
     required: true,
+    description: 'The address of the shop.',
+    postgresql: {
+      columnName: 'address',
+      dataType: 'text',
+      nullable: 'NO',
+    },
   })
   address: string;
 
@@ -33,6 +61,12 @@ export class Shop extends Entity {
     required: true,
     minLength: 10,
     maxLength: 10,
+    description: 'The telephone number of the shop.',
+    postgresql: {
+      columnName: 'tel',
+      dataType: 'text',
+      nullable: 'YES',
+    },
     errorMessage: {
       minLength: 'Telephone should be at least 10 characters.',
       maxLength: 'Telephone should not exceed 10 characters.',
@@ -43,8 +77,14 @@ export class Shop extends Entity {
   @property({
     type: 'string',
     required: false,
+    description: 'The status of the shop.',
     jsonSchema: {
       enum: Object.values(StatusEnum),
+    },
+    postgresql: {
+      columnName: 'status',
+      dataType: 'text',
+      nullable: 'YES',
     },
   })
   status?: StatusEnum;
@@ -52,6 +92,12 @@ export class Shop extends Entity {
   @property({
     type: 'number',
     required: false,
+    description: 'The ID of the shop owner.',
+    postgresql: {
+      columnName: 'ownerId',
+      dataType: 'integer',
+      nullable: 'YES',
+    },
   })
   ownerId?: number;
 

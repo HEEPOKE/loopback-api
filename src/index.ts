ffcee6a4f7ee13af78425fdc2fe1b295750dbd1d
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-09-15 10:52:11 +0700

    :money_with_wings: set cors && basepath

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import {RestBindings} from '@loopback/rest';
 import {AppApplication, ApplicationConfig} from './application';
 import ConfigApp from './configs/config';
 
@@ -8,6 +9,10 @@ export async function main(options: ApplicationConfig = {}) {
   await app.boot();
   await app.migrateSchema();
   await app.start();
+
+  app
+    .bind(RestBindings.ERROR_WRITER_OPTIONS)
+    .to({safeFields: ['name', 'message', 'code', 'service']});
 
   const url = app.restServer.url;
   console.log(`Server is running at ${url}`);
@@ -20,9 +25,14 @@ if (require.main === module) {
     rest: {
       port: +(ConfigApp.PORT ?? 3000),
       host: ConfigApp.HOST,
+      basePath: '/apis',
       gracePeriodForClose: 5000,
       openApiSpec: {
         setServersFromRequest: true,
+      },
+      cors: {
+        origin: '*',
+        methods: 'GET,HEAD,PUT,POST,DELETE',
       },
     },
   };
